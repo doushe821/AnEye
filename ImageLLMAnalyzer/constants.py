056523cbfd2e36eb95f46939ffe33b70660b530b
Barkir <r.barinov2006@gmail.com>
2025-12-03 12:49:15 +0300

    Edited prompt

--- ImageLLMAnalyzer/constants.py
+++ ImageLLMAnalyzer/constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 DOCS_EXTENSIONS = [".pdf"]
 IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif", ".bmp", "webp"]
-TECHNICAL_PROMPT =  "ON THE FIRST LINE OF YOUR ANSWER PLACE THE OFFICIAL NAME OF THE DEVICE. " \
+TECHNICAL_PROMPT =  "ON THE FIRST LINE OF YOUR ANSWER PLACE THE OFFICIAL NAME OF THE DEVICE IN FORMAT [NAME_OF_THE_DEVICE]" \
 "IF YOU DON'T KNOW THE DEVICE, LET THE FIRST LINE BE 'UNKNOW DEVICE'" \
 "I need technical information about what is on photo. " \
 "You need to provide valuable information for an engineer." \
